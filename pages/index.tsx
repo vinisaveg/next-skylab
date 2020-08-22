0d0a4fc96a6b5e4f1ee4fa41f7136e36eb331e71
savegnago <savegdesigner@gmail.com>
2020-08-22 15:14:12 -0300

    Project styled :nail_care:

---
+++ pages/index.tsx
@@ -0,0 +1,104 @@
+// import Head from "next/head"
+
+import { Heading, Grid, Flex, Input, Link, Button, Text } from "@chakra-ui/core"
+
+import Divider from "../components/Divider"
+
+export default function Home() {
+  return (
+    <Grid
+      as="main"
+      height="100vh"
+      templateColumns="1fr 480px 480px 1fr"
+      templateRows="1fr 480px 1fr"
+      templateAreas="
+       '. . . .'
+       '. logo form .'
+       '. . . .'
+      "
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Flex gridArea="logo" flexDir="column" alignItems="flex-start">
+        <img src="/rocketseat.svg" alt="Rocketseat" />
+
+        <Heading size="2xl" lineHeight="shorter" marginTop={16}>
+          Faça seu login na plataforma
+        </Heading>
+      </Flex>
+
+      <Flex
+        gridArea="form"
+        height="100%"
+        backgroundColor="gray.700"
+        borderRadius="md"
+        flexDir="column"
+        alignItems="stretch"
+        padding={16}
+      >
+        <Input
+          height="50px"
+          backgroundColor="gray.800"
+          focusBorderColor="purple.500"
+          borderRadius="sm"
+          placeholder="E-mail"
+        />
+
+        <Input
+          height="50px"
+          backgroundColor="gray.800"
+          focusBorderColor="purple.500"
+          borderRadius="sm"
+          placeholder="Senha"
+          marginTop={2}
+          type="password"
+        />
+
+        <Link
+          alignSelf="flex-start"
+          marginTop={2}
+          fontSize="sm"
+          fontWeight="bold"
+          color="purple.600"
+          _hover={{ color: "purple.500" }}
+        >
+          Esqueci minha senha
+        </Link>
+
+        <Button
+          backgroundColor="purple.500"
+          height="50px"
+          borderRadius="sm"
+          _hover={{ backgroundColor: "purple.600" }}
+          marginTop={6}
+        >
+          Entrar
+        </Button>
+
+        <Text textAlign="center" fontSize="sm" color="gray.300" marginTop={6}>
+          Não tem uma conta?{" "}
+          <Link fontWeight="bold" color="purple.600" _hover={{ color: "purple.500" }}>
+            Registre-se
+          </Link>
+        </Text>
+
+        <Divider />
+
+        <Flex alignItems="center">
+          <Text fontSize="sm">Ou entre com</Text>
+
+          <Button
+            height="50px"
+            flex="1"
+            backgroundColor="gray.600"
+            marginLeft={6}
+            borderRadius="sm"
+            _hover={{ backgroundColor: "purple.500" }}
+          >
+            Github
+          </Button>
+        </Flex>
+      </Flex>
+    </Grid>
+  )
+}
